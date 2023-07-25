1abe2734e7e7ac68894f43025d7036dc92c03a9c
arklual <arklual1@gmail.com>
2023-07-25 12:37:24 +0300

    fix

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import aiofiles
 from settings import *
 
 app = Client("bot", api_id=api_id, api_hash=api_hash)
+app.start()
 zone = zoneinfo.ZoneInfo("Europe/Moscow")
 gc = pygsheets.authorize(service_account_file='sheets_key.json')
 ws = gc.open('База писывших людей').worksheet()
@@ -24,3 +25,4 @@ for dialog in app.get_dialogs(limit=100):
         if str(dialog.chat.id) not in ids:
             ids.append(str(dialog.chat.id))
             ws.update_row(len(ids), [dialog.chat.id, dialog.chat.first_name, dialog.chat.last_name, dialog.chat.username, '', str(datetime.now(zone).date())]) 
+app.stop()
